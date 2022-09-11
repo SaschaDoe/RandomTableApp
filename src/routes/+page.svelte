043c8813cb20ea3c209a531750debd8b860f7bad
Sascha Dörflein <48054686+SaschaDoe@users.noreply.github.com>
2022-09-11 05:44:46 +0200

    Add netlify.toml and adapter for deployment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Random Tables</h1>
+<p>Visit <a href="https://github.com/SaschaDoe/RandomTableApp">SaschaDoe's github</a> for the source code</p>
