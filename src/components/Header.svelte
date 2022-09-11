d7ee0af6c31bad74121c93f34d85d10be3ea8808
Sascha Dörflein <48054686+SaschaDoe@users.noreply.github.com>
2022-09-11 06:17:51 +0200

    Added Header and workaround for https://github.com/sveltejs/kit/issues/6462

---
+++ src/components/Header.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+
+</script>
+
+<header>
+    <h1>
+        <button class="random-table-btn">Random Tables</button>
+        <button class="summary-btn">Summary</button>
+    </h1>
+</header>
+
+<style>
+    header{
+        background: darkgrey;
+        padding: 5px;
+    }
+    button{
+        background-color: transparent;
+        background-repeat: no-repeat;
+        border: none;
+        cursor: pointer;
+        overflow: hidden;
+        outline: none;
+        font-weight: bold;
+    }
+    .random-table-btn{
+        position: absolute;
+        left: 30%;
+        top: 15px;
+    }
+    .summary-btn{
+        position: absolute;
+        left: 60%;
+        top: 15px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Random Tables</h1>
-<p>Visit <a href="https://github.com/SaschaDoe/RandomTableApp">SaschaDoe's github</a> for the source code</p>
+<script>
+    import Header from '../components/Header.svelte'
+</script>
+
+<Header></Header>
+<main>
+    <h1>Random Tables</h1>
+    <p>Visit <a href="https://github.com/SaschaDoe/RandomTableApp">SaschaDoe's github</a> for the source code</p>
+</main>
+
+<style>
+    main{
+        max-width: 960px;
+    }
+</style>
