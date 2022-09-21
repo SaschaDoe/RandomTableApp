cdf812b8690074a12bb9517cbd8579cebda474fc
Sascha Dörflein <48054686+SaschaDoe@users.noreply.github.com>
2022-09-21 10:00:46 +0200

    reproduction infertile fix

--- src/tables/monsterTables/monsterReproductionTable.ts
+++ src/tables/monsterTables/monsterReproductionTable.ts
@@ -12,7 +12,7 @@ export class MonsterReproductionTable extends Table{
         entries.push(new TableEntry("division"))
         entries.push(new TableEntry("self fertilization"))
         entries.push(new TableEntry("parasite"))
-        entries.push(new TableEntry("infertile"))
+        entries.push(new TableEntry("not it is infertile"))
         entries.push(new TableEntry("eggs under water"))
         entries.push(new TableEntry("bag"))
         super(entries, TableTitles.MonsterReproduction,TableType.Character);
